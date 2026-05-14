e72647755d4dbe32462bdbad7e7ea21544339e61
David Chen <dchen.dev71@gmail.com>
2026-05-14 17:28:44 +0200

    add: PasswordEncoder

--- backend/src/main/java/com/example/springboot/user/UserService.java
+++ backend/src/main/java/com/example/springboot/user/UserService.java
@@ -7,21 +7,27 @@ import com.example.springboot.user.dto.UserCreateRequest;
 import com.example.springboot.user.dto.UserResponse;
 import com.example.springboot.user.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
+  private final PasswordEncoder passwordEncoder;
   private final UserRepository userRepository;
   private final UserMapper userMapper;
   private final CurrencyService currencyService;
 
   @Autowired
   public UserService(
-      UserRepository userRepository, UserMapper userMapper, CurrencyService currencyService) {
+      UserRepository userRepository,
+      UserMapper userMapper,
+      CurrencyService currencyService,
+      PasswordEncoder passwordEncoder) {
     this.userRepository = userRepository;
     this.userMapper = userMapper;
     this.currencyService = currencyService;
+    this.passwordEncoder = passwordEncoder;
   }
 
   @Transactional
@@ -31,6 +37,9 @@ public class UserService {
       throw new EmailAlreadyExistsException(request.email());
     }
     User user = userMapper.toEntity(request, currency);
+
+    String encodedPwd = passwordEncoder.encode(request.password());
+    user.setPassword(encodedPwd);
 
     return userMapper.toResponse(userRepository.save(user));
   }
--- backend/src/test/java/com/example/springboot/user/UserServiceTest.java
+++ backend/src/test/java/com/example/springboot/user/UserServiceTest.java
@@ -20,11 +20,13 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
 
   @Mock private UserRepository userRepository;
+  @Mock private PasswordEncoder passwordEncoder;
 
   @Mock private CurrencyService currencyService;
 
