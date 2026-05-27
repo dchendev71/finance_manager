eaa4cebc7d86f328ead581dacba8eaefb29d54cf
David Chen <dchen.dev71@gmail.com>
2026-05-27 14:12:42 +0200

    frontend: Add asset row display

--- frontend/src/components/portfolio/Portfolio.tsx
+++ frontend/src/components/portfolio/Portfolio.tsx
@@ -1,5 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AssetRowForm from "./asset_row/AssetRowForm";
+import AssetRow from "./asset_row/AssetRow";
+import { useAuth } from "@/components/auth/AuthContext";
 
 export interface PortfolioProps {
   portfolioName: string;
@@ -9,6 +11,28 @@ export default function Portfolio({ portfolioName }: PortfolioProps) {
   const [showForm, setShowForm] = useState<boolean>(false);
   const [error, setError] = useState<string>("");
   const [assetRowList, setAssetRowList] = useState([]);
+  const { request } = useAuth();
+
+  useEffect(() => {
+    async function getAssetRowList() {
+      try {
+        const response = await request(
+          `/portfolio/portfolio-asset/list/${portfolioName}`,
+        );
+        if (response) {
+          // First trim the response to just get the assetRow
+          const cleanedList = response.map(
+            (assetRowResponse: any) => assetRowResponse.assetResponse,
+          );
+          setAssetRowList(cleanedList);
+        }
+      } catch (e: any) {
+        setError(e.message || "Network error - please try again");
+      }
+    }
+
+    getAssetRowList();
+  }, []);
 
   async function handleClick() {
     setShowForm((prev) => !prev);
@@ -28,6 +52,11 @@ export default function Portfolio({ portfolioName }: PortfolioProps) {
       ) : (
         <>
           <h1>{portfolioName}</h1>
+          {assetRowList.length > 0 &&
+            assetRowList.map((assetRow: any) => (
+              <AssetRow key={assetRow.name} assetName={assetRow.name} />
+            ))}
+
           <button onClick={handleClick}>Add asset</button>
         </>
       )}
--- frontend/src/components/portfolio/asset_row/AssetRow.tsx
+++ frontend/src/components/portfolio/asset_row/AssetRow.tsx
@@ -1,3 +1,6 @@
-export default function AssetRow() {
-  return <h1>Hello from AssetRow!</h1>;
+interface AssetRowProps {
+  assetName: string;
+}
+export default function AssetRow({ assetName }: AssetRowProps) {
+  return <h1>Hello from {assetName}!</h1>;
 }
