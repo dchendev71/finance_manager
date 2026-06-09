89b25225b228338cb142fb3f8c0ed8a9efffcc4e
David Chen <dchen.dev71@gmail.com>
2026-06-09 17:48:57 +0200

    frontend: Fix conflict on UserPage

--- frontend/src/components/user/UserPage.tsx
+++ frontend/src/components/user/UserPage.tsx
@@ -1,11 +1,6 @@
 import Button from "@/components/ui/Button";
 import { useUser } from "./UserContext";
 import { formatToUTCLibrary } from "@/services/api";
-<<<<<<< Updated upstream
-
-export default function UserPage() {
-  const { user } = useUser();
-=======
 import { useState } from "react";
 import ChangePasswordForm from "./forms/ChangePasswordForm";
 import ChangeEmailForm from "./forms/ChangeEmailForm";
@@ -65,7 +60,6 @@ export default function UserPage() {
         return <ChangeCurrencyForm />;
     }
   };
->>>>>>> Stashed changes
   return (
     <div className="min-h [calc(100vh-4rem)] bg-slate-100 text-slate-950 p-8">
       <div className="max-w-5xl mx-auto">
@@ -78,54 +72,6 @@ export default function UserPage() {
 
         <div className="grid grid-cols-3 gap-8 item-start">
           <div className="bg-slate-100 border border-slate-800 rounded-xl p-4 flex flex-col space-y-1">
-<<<<<<< Updated upstream
-            <Button value="Change Email" variant="blue" />
-            <Button value="Change Password" variant="blue" />
-            <Button value="Go to Dashboard" variant="blue" />
-            <Button value="Portfolios" variant="blue" />
-            <Button value="Performance" variant="blue" />
-            <Button value="Change Currency" variant="blue" />
-          </div>
-          <div className="col-span-2 bg-slate-100 border border-slate-800 rounded-xl p-6 min-h-[300px]">
-            <dl className="flex flex-col gap-4">
-              <div>
-                <dt className="text-xs text-slate-500">Email</dt>
-                <dd className="text-sm font-semibold">{user?.email}</dd>
-              </div>
-
-              <div>
-                <dt className="text-xs text-slate-500">Currency</dt>
-                <dd className="text-sm font-semibold">
-                  <dl className="flex flex-row gap-4">
-                    <div>
-                      <dt className="text-xs text-slate-400">Name</dt>
-                      <dd className="text-sm font-semibold">
-                        {user?.currency.name}
-                      </dd>
-                    </div>
-                    <div>
-                      <dt className="text-xs text-slate-400">Code</dt>
-                      <dd className="text-sm font-semibold">
-                        {user?.currency.code}
-                      </dd>
-                    </div>
-                    <div>
-                      <dt className="text-xs text-slate-400">Symbol</dt>
-                      <dd className="text-sm font-semibold">
-                        {user?.currency.symbol}
-                      </dd>
-                    </div>
-                  </dl>
-                </dd>
-              </div>
-              <div>
-                <dt className="text-xs text-slate-500">Created at</dt>
-                <dd className="text-sm font-semibold">
-                  {formatToUTCLibrary(user?.createdAt)}
-                </dd>
-              </div>
-            </dl>
-=======
             <Button
               value="Profile Details"
               variant="green"
@@ -152,7 +98,6 @@ export default function UserPage() {
           </div>
           <div className="col-span-2 bg-slate-100 border border-slate-800 rounded-xl p-6 min-h-[300px]">
             {renderDashboard()}
->>>>>>> Stashed changes
           </div>
         </div>
       </div>
